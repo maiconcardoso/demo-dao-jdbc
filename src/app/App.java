8151e6f7971d57c385e81aec491535092aba7348
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-08-06 06:44:34 -0300

    Seller entity class

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,16 @@
 package app;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
-        System.out.println(obj);
+        System.out.println(seller);
     }
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import db.exceptions.DbException;
+
 public class DB {
     private static String driver = "com.mysql.cj.jdbc.Driver";
     private static String url = "jdbc:mysql://localhost:3306/coursejdbc?useTimeZone=true&serverTimeZone=UTC";
