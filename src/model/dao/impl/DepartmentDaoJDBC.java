8475cae032781042a9514192b4c8ab0510e97c27
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-08-08 06:50:24 -0300

    Department: findById implementation

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,69 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.exceptions.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao{
+
+    private Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void update(Department obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        String sql = "SELECT * FROM department WHERE Id = ?";
+
+        try {
+            st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            rs = st.executeQuery();
+
+            if (rs.next()) {
+                Department department = new Department();
+                department.setId(rs.getInt("Id"));
+                department.setName(rs.getString("Name"));
+                return department;
+            }
+
+        } catch (SQLException e) {
+            throw new DbException("Error: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+}
